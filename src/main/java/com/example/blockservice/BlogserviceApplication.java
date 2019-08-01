84f16d7e99f7d23b5d8a4ca92ecdc6fa29368030
Rahardyan Bisma <rahardyanbismasetya.putra@mitrais.com>
2019-08-01 11:40:14 +0700

    initial commit

--- src/main/java/com/example/blockservice/BlogserviceApplication.java
+++ src/main/java/com/example/blockservice/BlogserviceApplication.java
@@ -1,13 +1,13 @@
-package com.example.blogrest;
+package com.example.blockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BlogserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BlogserviceApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/blockservice/BlogserviceApplicationTests.java
+++ src/test/java/com/example/blockservice/BlogserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.blogrest;
+package com.example.blockservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class BlogserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
